c1a6f8bd2571da3e434c300c1ae7731d740c2045
IgorDv5 <igoraluno2005@gmail.com>
2026-05-21 20:23:22 -0300

    feat: models Transaction Implementado

---
+++ src/app/shared/models/transaction/Transaction.ts
@@ -0,0 +1,15 @@
+import { TransactionType } from "./enums/transaction-type.enum";
+
+
+export interface Transaction {
+
+  id: number;
+  amount: number;
+  description: string;
+  date: string;
+  type: TransactionType;
+  deletedAt?: string;
+  categoryId: number;
+  userId: number;
+
+}
