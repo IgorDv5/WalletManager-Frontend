3b035814c3d6983e981c268afdc3c4b69d0bf9f0
IgorDv5 <igoraluno2005@gmail.com>
2026-05-21 20:22:44 -0300

    feat: transactionService Implementaddo

---
+++ src/app/core/services/transaction.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Transaction } from '../../shared/models/transaction/Transaction';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransactionService {
+
+  private http = inject(HttpClient);
+  private baseURL = 'http://localhost:8080/transactions';
+
+  findById(id:number): Observable<Transaction>{
+    return this.http.get<Transaction>(`${this.baseURL}/${id}`);
+  }
+
+  findAll(): Observable<Transaction[]>{
+    return this.http.get<Transaction[]>(`${this.baseURL}`);
+  }
+
+  create(transaction : Transaction): Observable<Transaction>{
+    return this.http.post<Transaction>(`${this.baseURL}`,transaction);
+  }
+
+  update(transaction : Transaction): Observable<Transaction>{
+    return this.http.put<Transaction>(`${this.baseURL}/${transaction.id}`, transaction);
+  }
+
+}
