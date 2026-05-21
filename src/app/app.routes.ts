75390965db1bb2706855f2a03f6d177a64523c73
IgorDv5 <igoraluno2005@gmail.com>
2026-05-21 19:44:49 -0300

    feat: Componente home implementado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
 
     {
         path: '',
-        component: FooterComponent
+        component: HomeComponent
     }
 
 ];
