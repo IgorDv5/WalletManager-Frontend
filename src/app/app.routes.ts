f5b6461226a55402878d9941a770c6fbb3a38d1c
IgorDv5 <igoraluno2005@gmail.com>
2026-05-21 19:39:18 -0300

    feat: Footer componente implementado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 export const routes: Routes = [
 
     {
         path: '',
-        component: HeaderComponent
+        component: FooterComponent
     }
 
 ];
