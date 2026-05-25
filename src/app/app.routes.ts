912bd9ee27bae584eec0ff9fef8eed84e3e23a0f
IgorDv5 <igoraluno2005@gmail.com>
2026-05-25 18:57:20 -0300

    fix: Remoção de Header e footer na rota de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,68 +1,42 @@
 import { Routes } from '@angular/router';
+
 import { HomeComponent } from './pages/home/home.component';
+
 import { TransactionListComponent } from './pages/transactions/transaction-list/transaction-list.component';
+import { TransactionCreateComponent } from './pages/transactions/transaction-create/transaction-create.component';
+import { TransactionEditComponent } from './pages/transactions/transaction-edit/transaction-edit.component';
+
 import { CategoryListComponent } from './pages/categories/category-list/category-list.component';
 import { CategoryCreateComponent } from './pages/categories/category-create/category-create.component';
 import { CategoryEditComponent } from './pages/categories/category-edit/category-edit.component';
-import { TransactionCreateComponent } from './pages/transactions/transaction-create/transaction-create.component';
-import { TransactionEditComponent } from './pages/transactions/transaction-edit/transaction-edit.component';
+
 import { UserListComponent } from './pages/users/user-list/user-list.component';
-import { UserEditComponent } from './pages/users/user-edit/user-edit.component';
 import { UserCreateComponent } from './pages/users/user-create/user-create.component';
+import { UserEditComponent } from './pages/users/user-edit/user-edit.component';
+
 import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [
+import { authGuard } from './core/auth/authGuard';
+import { LayoutComponent } from './shared/layout/layout.component';
 
-    {
-        path: '',
-        component: HomeComponent
-    },
+export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
     },
-    {
-        path: 'home',
-        component: HomeComponent
-    },
-    // ******Users**********
-    {
-        path: 'users',
-        component: UserListComponent
-    },
-    {
-        path: 'users/create',
-        component: UserCreateComponent
-    },
-    {
-        path: 'users/edit/:id',
-        component: UserEditComponent
-    },
-    // ******Categories**********
-    {
-        path: 'categories',
-        component: CategoryListComponent
-    },
-    {
-        path: 'categories/create',
-        component: CategoryCreateComponent
-    },
-    {
-        path: 'categories/edit/:id',
-        component: CategoryEditComponent
-    },
-    // ******Transactions**********
-    {
-        path: 'transactions',
-        component: TransactionListComponent
-    },
-    {
-        path: 'transactions/create',
-        component: TransactionCreateComponent
-    },
-    {
-        path: 'transactions/edit/:id',
-        component: TransactionEditComponent
-    }
 
+    {
+        path: '',
+        component: LayoutComponent,
+        canActivate: [authGuard],
+        children: [
+            { path: 'home', component: HomeComponent },
+            { path: '', component: HomeComponent },
+            { path: 'transactions', component: TransactionListComponent },
+            { path: 'transactions/create', component: TransactionCreateComponent },
+            { path: 'transactions/edit/:id', component: TransactionEditComponent },
+            { path: 'categories', component: CategoryListComponent },
+            { path: 'users', component: UserListComponent }
+        ]
+    }
 ];
---
+++ src/app/core/auth/authGuard.ts
@@ -0,0 +1,18 @@
+import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const authenticated = authService.isAuthenticated();
+
+  if (authenticated) {
+    return true;
+  }
+
+  router.navigate(['/login']);
+  return false;
+};
